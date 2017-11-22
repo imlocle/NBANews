2867c2e7a39b03cfabc9027c2da97fef8be85acb
Loc <loc.tan.le@gmail.com>
2017-11-22 05:34:39 -0800

    basic set up for login reg. Models, Urls, Views, Settings, Index, Loginreg

--- apps/nba_news/models.py
+++ apps/nba_news/models.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+import re
+import bcrypt
+from datetime import datetime
 
-# Create your models here.
+class UserManager(models.Manager):
+    def registration(self, name, email, password, confirm_password):
+        errorlist = []
+        count = 0
+        now = datetime.now()
+        if len(name) < 2:
+            errorlist.append("First name is too short")
+            count += 1
+        if len(email) < 1:
+            errorlist.append("Email is required!")
+            count += 1
+            
--- apps/nba_news/views.py
+++ apps/nba_news/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import re
+from django.shortcuts import render, redirect
+from django.contrib import messages
+import bcrypt
 
-from django.shortcuts import render
+def index(request):
+    return render(request, 'nba_news/index.html')
 
-# Create your views here.
+def registration(request):
+    
