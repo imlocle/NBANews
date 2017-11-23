9dd7c527f278cdebd1e4c7b96ce690c796b97b2c
Loc Le <loc.tan.le@gmail.com>
2017-11-22 23:40:15 -0800

    Models and Views Log in Register

--- apps/nba_news/models.py
+++ apps/nba_news/models.py
@@ -1,25 +1,55 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+from django.contrib import messages
 import re
 import bcrypt
-from datetime import datetime
+import datetime
 
-EMAILCHECK = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 class UserManager(models.Manager):
-    def registration(self, name, email, password, confirm_password):
+    def register(self, first_name, last_name, email, password, confirm_password):
         errorlist = []
-        count = 0
-        now = datetime.now()
-        if len(name) < 2:
-            errorlist.append("First name is too short")
-            count += 1
-        if len(email) < 1:
-            errorlist.append("Email is required!")
-            count += 1
-        elif not  EMAILCHECK.match(email):
-            errorlist.append('Please enter a valid email')
-            count += 1
-        if not password == confirm_password:
-            errorlist.append("")
+        the_user = User.objects.filter(email = email)
+
+        if len(first_name) < 2:
+            errorlist.append("First name is too short")            
+        if len(last_name) < 2:
+            errorlist.append("Last name is too short")           
+        if not  EMAIL_REGEX.match(email):
+            errorlist.append('Please enter a valid email') 
+        if the_user:
+            errorlist.append("Email already taken")           
+        if confirm_password != password:
+            errorlist.append("Passwords don't match")
+        if len(password) < 8:
+            errorlist.append("Please enter a password longer than 8 characters")
+        if len(errorlist) > 0:
+            return errorlist
+        else:
+            return True
+
+    def login(self, email, password):
+        errorlist = []
+        the_user = User.objects.filter(email = email)
+        if the_user:
+            if bcrypt.hashpw(password.encode('utf-8'), the_user[0].password.encode('utf-8')) == the_user[0].password:
+                return True
+            else:
+                errorlist.append("Wrong email or password")
+                return errorlist
+        else:
+            errorlist.append("Wrong email or password")
+            return errorlist
+
+class User(models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    email = models.EmailField()
+    password = models.CharField(max_length = 100)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    objects = UserManager()
+
+
--- apps/nba_news/views.py
+++ apps/nba_news/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import re
 from django.shortcuts import render, redirect
+from .models import User, UserManager
 from django.contrib import messages
 import bcrypt
 
@@ -9,4 +9,41 @@ def index(request):
     return render(request, 'nba_news/index.html')
 
 def registration(request):
-    
+    check = User.objects.register(
+        request.POST["first_name"]
+        , request.POST["last_name"]
+        , request.POST["email"]
+        , request.POST["password"]
+        , request.POST["confirm_password"])
+
+    if check == True:
+        passwordinput = request.POST["password"].encode('utf-8')
+        hashed = bcrypt.hashpw(passwordinput, bcrypt.gensalt())
+        user = User.objects.create(
+            first_name = request.POST["first_name"]
+            , last_name = request.POST["last_name"]
+            , email = request.POST["email"]
+            , password = hashed)
+        request.session["current_user"] = user.id
+        return redirect("/nba_news")
+    else:
+        for i in range(0, len(check)):
+            messages.warning(request, check[i])
+        return redirect("/")
+
+def login(request):
+    check = User.objects.login(
+        request.POST["email"]
+        , request.POST["password"])
+
+    if check == True:
+        user = User.objects.get(email = request.POST['email'])
+        request.session["current_user"] = user.id
+        return redirect('/nba_news')
+    else:
+        messages.warning(request, check[0])
+        return redirect('/')
+
+def logoutr(request):
+    request.session['current_user'] = 0
+    return redirect('/')
